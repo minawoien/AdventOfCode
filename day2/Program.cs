054bc0540a289da407eba14c82e4adb0d279c5bb
minawoien <mc.woien@stud.uis.no>
2022-12-04 16:22:54 +0100

    day2

---
+++ day2/Program.cs
@@ -0,0 +1,56 @@
+﻿int sum = 0;
+
+var values = new Dictionary<string, int>(){
+    {"X", 1},
+    {"Y", 2},
+    {"Z", 3}
+};
+
+var roles = new Dictionary<int, string>(){
+    { 3, "AX, BY, CZ"},
+    { 6, "AY, BZ, CX"},
+    { 0, "AZ, BX, CY"}
+};
+
+string[] lines = System.IO.File.ReadAllLines(@"input.txt");
+foreach (var item in lines)
+{
+    var round = item.Split(" ");
+    sum += values[round[1]];
+    foreach (var role in roles)
+    {
+        if (role.Value.Contains(round[0] + round[1]))
+        {
+            sum += role.Key;
+        }
+    }
+}
+
+Console.WriteLine(sum);
+
+
+// Part two
+var newRoles = new Dictionary<string, int>(){
+    {"X", 0},
+    {"Y", 3},
+    {"Z", 6}
+};
+
+int newSum = 0;
+foreach (var item in lines)
+{
+    var round = item.Split(" ");
+    int key = newRoles[round[1]];
+    newSum += key;
+    var pairList = roles[key].Split(",");
+    foreach (var pair in pairList)
+    {
+        if (pair.Contains(round[0]))
+        {
+            var last = pair.Last().ToString();
+            newSum += values[last];
+        }
+
+    }
+}
+Console.WriteLine(newSum);
