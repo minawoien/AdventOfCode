42730eecd4b34ca032a5bb20d29ed60eec74fa89
minawoien <mc.woien@stud.uis.no>
2022-12-06 08:15:53 +0100

    Read the file as one string

--- day6/Program.cs
+++ day6/Program.cs
@@ -1,8 +1,5 @@
-﻿string[] lines = System.IO.File.ReadAllLines(@"input.txt");
-foreach (var line in lines)
-{
-    int start_of_packet = Charnumb.NumberOfChar(line, 4);
-    int start_of_message = Charnumb.NumberOfChar(line, 14);
-    Console.WriteLine($"The number of characters that needs to be processed before the first start-of-packet marker is detected is {start_of_packet}");
-    Console.WriteLine($"The number of characters that needs to be processed before the first start-of-message marker is detected is {start_of_message}");
-}
+﻿string line = System.IO.File.ReadAllText(@"input.txt");
+int start_of_packet = Charnumb.NumberOfChar(line, 4);
+int start_of_message = Charnumb.NumberOfChar(line, 14);
+Console.WriteLine($"The number of characters that needs to be processed before the first start-of-packet marker is detected is {start_of_packet}");
+Console.WriteLine($"The number of characters that needs to be processed before the first start-of-message marker is detected is {start_of_message}");
