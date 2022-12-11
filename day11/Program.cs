875bf2792ca6a4abc648894f98eda1b32f76eef9
minawoien <mc.woien@stud.uis.no>
2022-12-11 23:09:34 +0100

    create new function to sort input from file

--- day11/Program.cs
+++ day11/Program.cs
@@ -1,17 +1,15 @@
 ﻿string[] lines = System.IO.File.ReadAllLines(@"input.txt");
 Dictionary<int, List<double>> worry_levels = new Dictionary<int, List<double>>();
 Dictionary<int, double> inspected = new Dictionary<int, double>();
-int current_monkey = 0;
 List<double> divisors = new List<double>();
 for (var i = 0; i < lines.Length; i += 7)
 {
-    // Find current monkey
+    // Find Starting items and sort input from file
     var item = lines[i].Split(" ");
-    current_monkey = Int32.Parse(item[1].Remove(item[1].Length - 1, 1));
-
-    // Find Starting items
     var start = lines[i + 1].Split(":");
     var starting_items = start[1].Split(",");
+    (int current_monkey, string[] ch, double numb) = sort_input(lines, i);
+
     List<double> start_items = new List<double>();
     foreach (var worry_level in starting_items)
     {
@@ -20,9 +18,7 @@ for (var i = 0; i < lines.Length; i += 7)
     worry_levels.Add(current_monkey, start_items);
     inspected.Add(current_monkey, 0);
 
-    // Find devisors
-    var test = lines[i + 3].Split(" ");
-    divisors.Add(double.Parse(test[test.Length - 1]));
+    divisors.Add(numb); // Find devisors
 }
 double lcm_divisors = LCM(divisors);
 
@@ -31,25 +27,15 @@ for (var j = 0; j < 10000; j++)
     for (var i = 0; i < lines.Length; i += 7)
     {
         double count = 0;
-        // Find current monkey and its items
-        var item = lines[i].Split(" ");
-        current_monkey = Int32.Parse(item[1].Remove(item[1].Length - 1, 1));
+        // Sort input from file
+        (int current_monkey, string[] ch, double numb) = sort_input(lines, i);
         var starting_items = worry_levels[current_monkey];
-
-        // Find operation
-        var operation = lines[i + 2].Split(":");
-        var ch = operation[1].Split(" ");
-        double x = 0;
-
-        // Find test
-        var test = lines[i + 3].Split(" ");
-        double numb = double.Parse(test[test.Length - 1]);
-
         // Find next monkey
         var next_true = lines[i + 4].Split(" ");
         var t_next = Int32.Parse(next_true[next_true.Length - 1]);
         var next_false = lines[i + 5].Split(" ");
         var f_next = Int32.Parse(next_false[next_false.Length - 1]);
+        double x = 0;
         foreach (var worry_level in starting_items)
         {
             double new_worry = calculate_worry_level(x, worry_level, ch);
@@ -69,19 +55,15 @@ for (var j = 0; j < 10000; j++)
         inspected[current_monkey] += count;
     }
 }
-
-Console.WriteLine(sortDict(inspected));
+var inspected_list = inspected.ToList();
+inspected_list.Sort((pair1, pair2) => pair1.Value.CompareTo(pair2.Value));
+inspected_list.Reverse();
+Console.WriteLine(inspected_list[0].Value * inspected_list[1].Value);
 
 static double gcd(double n1, double n2)
 {
-    if (n2 == 0)
-    {
-        return n1;
-    }
-    else
-    {
-        return gcd(n2, n1 % n2);
-    }
+    if (n2 == 0) { return n1; }
+    else { return gcd(n2, n1 % n2); }
 }
 
 static double LCM(List<double> numbers)
@@ -103,10 +85,19 @@ static double calculate_worry_level(double x, double worry_level, string[] ch)
     }
 }
 
-static double sortDict(Dictionary<int, double> dict)
+static (int, string[], double) sort_input(string[] lines, int i)
 {
-    var inspected_list = dict.ToList();
-    inspected_list.Sort((pair1, pair2) => pair1.Value.CompareTo(pair2.Value));
-    inspected_list.Reverse();
-    return inspected_list[0].Value * inspected_list[1].Value;
+    // Find current monkey
+    int current_monkey = 0;
+    var item = lines[i].Split(" ");
+    current_monkey = Int32.Parse(item[1].Remove(item[1].Length - 1, 1));
+
+    // Find operation
+    var operation = lines[i + 2].Split(":");
+    var ch = operation[1].Split(" ");
+
+    // Find test
+    var test = lines[i + 3].Split(" ");
+    double numb = double.Parse(test[test.Length - 1]);
+    return (current_monkey, ch, numb);
 }
