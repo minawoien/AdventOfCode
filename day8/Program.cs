9e38b55481b6605c56f4247df447c31201c5583d
minawoien <mc.woien@stud.uis.no>
2022-12-28 17:39:56 +0100

    day8 part2

--- day8/Program.cs
+++ day8/Program.cs
@@ -99,3 +99,59 @@ for (var i = 1; i < lines.Length - 1; i += 1)
 }
 
 Console.WriteLine($"Threes visible from outside the grid is {visible.Count()}");
+
+
+// Part 2
+
+List<int> scenic_scores = new List<int>();
+// The possible places for the three house is inside the frame
+// as the view is zero for at least one side on the frame
+for (var i = 1; i < lines.Length - 1; i++)
+{
+    for (var j = 1; j < lines[i].Length - 1; j++)
+    {
+        int right = 0;
+        int left = 0;
+        int up = 0;
+        int down = 0;
+        // Look right
+        for (var k = j + 1; k < lines[i].Length; k++)
+        {
+            right++;
+            if (lines[i][j] <= lines[i][k])
+            {
+                break;
+            }
+        }
+        // Look left
+        for (var k = j - 1; k > -1; k--)
+        {
+            left++;
+            if (lines[i][j] <= lines[i][k])
+            {
+                break;
+            }
+        }
+        // Look up
+        for (var k = i - 1; k > -1; k--)
+        {
+            up++;
+            if (lines[i][j] <= lines[k][j])
+            {
+                break;
+            }
+        }
+        // Look down
+        for (var k = i + 1; k < lines.Length; k++)
+        {
+            down++;
+            if (lines[i][j] <= lines[k][j])
+            {
+                break;
+            }
+        }
+        scenic_scores.Add(left * right * up * down);
+    }
+}
+
+Console.WriteLine($"The highest scenic score possible for any tree is {scenic_scores.Max()}");
